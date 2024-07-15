669af6ba32e67c52a1d83c05c0a3467ccdbd5381
gillyspace27 <gilly@swri.org>
2024-07-15 14:40:14 -0600

    update xrt to real response functions, switch to temperature array from step sizes

--- EMToolKit/instruments/xrt.py
+++ EMToolKit/instruments/xrt.py
@@ -10,13 +10,13 @@ from astropy.nddata import StdDevUncertainty
 # as an EMToolKit data sequence -- the selection of maps appropriate for
 # DEMs, corresponding errors, temperature response
 # functions and corresponding (log) temperature arrays
-def xrt_wrapper(maps_in, temp_step_size=0.15):
+def xrt_wrapper(maps_in, temperature_array):
 	[maps,logts,tresps,errs] = [[],[],[],[]]
 	for i in range(0,len(maps_in)):
 		current_map = copy.deepcopy(maps_in[i])#.rotate(order=3)
 		current_map.meta['channel'] = current_map.meta['ec_fw1_']+'_'+current_map.meta['ec_fw2_']
 		if(not('detector' in current_map.meta)): current_map.meta['detector'] = 'XRT'
-		[logt,tresp] = xrt_temperature_response(current_map, step_size=temp_step_size)
+		[logt,tresp] = xrt_temperature_response(current_map, temperature_array=temperature_array)
 		if(len(tresp) == len(logt)):
 			maps.append(current_map)
 			errs.append(StdDevUncertainty(estimate_xrt_error(current_map)))
@@ -30,7 +30,45 @@ def estimate_xrt_error(map_in):
 		print("Estimate XRT error is a prototype and only empirically estimates Open Ti-poly uncertainties")
 	return ((np.abs(map_in.data.astype(np.float32))*25 + 25**2)**0.5)
 
-def xrt_temperature_response(map_in, step_size=0.2):
+
+
+
+
+def xrt_temperature_response(map_in, temperature_array):
+	import xrtpy
+	import matplotlib.pyplot as plt
+	import numpy as np
+	# The real temperature response functions are found here:
+	# https://xrtpy.readthedocs.io/en/stable/notebooks/computing_functions/temperature_response.html
+
+	# Available channels: "Al-mesh", "Al-poly", "C-poly", "Ti-poly", "Be-thin", "Be-med", "Al-med", "Al-thick", "Be-thick" , "Al-poly/Al-mesh", "Al-poly/Ti-poly", "Al-poly/Al-thick", "Al-poly/Be-thick" , "C-poly/Ti-poly"
+
+	channel = map_in.meta['ec_fw1_']+'/'+map_in.meta['ec_fw2_']
+	channel = channel.replace("Open/", "")
+	print("MAP FILTER IS : ", channel)
+	filter = channel
+	date_time = "2007-09-22T21:59:59"
+	Temperature_Response_Fundamental = xrtpy.response.TemperatureResponseFundamental(
+		filter, date_time, abundance_model="Photospheric"
+	)
+	temperature_response = Temperature_Response_Fundamental.temperature_response()
+	CHIANTI_temperature = Temperature_Response_Fundamental.CHIANTI_temperature
+	log_CHIANTI_temperature = np.log10(CHIANTI_temperature.value)
+	logt, tresp = interp1d_logt(log_CHIANTI_temperature, temperature_response.value, temperature_array)
+	plt.plot(logt, tresp, 'b')
+	plt.show()
+	print(len(logt), len(tresp))
+	print(type(logt), type(tresp))
+	return logt, tresp
+
+
+def xrt_temperature_response_old(map_in, step_size=0.2):
+	import xrtpy
+	import matplotlib.pyplot as plt
+	import numpy as np
+	# The real temperature response functions are found here:
+	# https://xrtpy.readthedocs.io/en/stable/notebooks/computing_functions/temperature_response.html
+
 	channel = map_in.meta['ec_fw1_']+'_'+map_in.meta['ec_fw2_']
 	refchannels = np.array(['Open_Ti_poly'])
 
@@ -40,8 +78,20 @@ def xrt_temperature_response(map_in, step_size=0.2):
 	tresp_table = np.array([[0.0], [0.5], [1.0], [2.0], [4.0], [6.0], [15.], [16.5], [15.], [7.0], [4.5], [3.5]])*1.0e-26
 
 	logt, tresp = interpolate_table(logt_table, tresp_table, step_size)
+	resp_out = tresp[:,np.where(refchannels == channel)].flatten()
 
-	return logt,tresp[:,np.where(refchannels == channel)].flatten()
+	plt.plot(logt, resp_out, 'r')
+	plt.show()
+
+	print(len(logt), len(resp_out))
+	print(type(logt), type(resp_out))
+	return logt, resp_out
+
+
+def interp1d_logt(logt, tresp, temperature_array):
+	new_tresp = np.interp(temperature_array, logt, tresp)
+	return temperature_array, new_tresp
+
 
 def interpolate_table(logt, table, step_size):
     new_logt = np.arange(np.min(logt), np.max(logt) + step_size, step_size)
@@ -72,7 +122,6 @@ def download_xrt_data(base_path, date, redownload=False):
 	paths = list_fits_files(xrt_data_dir, 'xrt')
 
 	print(f"Found {len(paths)} xrt images on disk.")
-
 
 	if len(paths) == 0 or redownload:
 		print(f"Searching for XRT images from {date}...")
