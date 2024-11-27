0f1162bf4a30ee54a350adb332630662877c0e14
gillyspace27 <gilly@swri.org>
2024-11-27 14:13:11 -0700

    add some examples

--- EMToolKit/examples.py
+++ EMToolKit/examples.py
@@ -4,5 +4,8 @@ def examples():
     url = "https://emtoolkit.readthedocs.io/en/latest/examples/GALLERY_HEADER.html"
     webbrowser.open(url)
 
+def run_example():
+    pass
+
 if __name__ == "__main__":
     examples()
